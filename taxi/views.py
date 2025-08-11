42912989eddee02308cd4a55459425c5bc2f305c
Hetmarum <kpxmedet@gmail.com>
2025-08-12 00:18:02 +0300

    unordered list to table

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -39,3 +40,9 @@ class DriverListView(ListView):
 
 class DriverDetailView(DetailView):
     model = Driver
+    queryset = Driver.objects.prefetch_related(
+        Prefetch(
+            "cars",
+            queryset=Car.objects.select_related("manufacturer")
+        )
+    )
